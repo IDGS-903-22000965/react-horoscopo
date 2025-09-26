006dacd052e3ebdc5a03b7b52f2bee83a2272e43
unknown <79228@alumnos.utleon.edu.mx>
2025-09-26 10:52:00 -0600

    Se acomodo el boton de nueva consulta

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pnradee2fjg"
+    "revision": "0.smnt0j12qn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
