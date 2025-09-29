043ceaf824fc81760ffaa2231259769bd9999f02
unknown <79228@alumnos.utleon.edu.mx>
2025-09-29 11:14:14 -0600

    Se arreglo el modal, ya no aparece mocho

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p653gbf9eb8"
+    "revision": "0.4vo4v4qj4bo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
